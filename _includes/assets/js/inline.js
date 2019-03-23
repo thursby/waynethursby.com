cb4986e5a6d7aab601ca48bf29345db4edaeb0f9
Wayne Thursby <wayne.thursby@1srq.com>
2019-03-23 17:20:50 +0000

    Added responsive elements and mobile-ready view for devices under 600px. No frameworks were added for these features.

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,36 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+
+function positionColumns(scroll_pos) {
+  if (window.innerWidth >= 600) {
+    var leftCol = document.getElementById("left-col");
+    var rightCol = document.getElementById("right-col");
+    if (scroll_pos >= 100) {
+      leftCol.classList.add("left-col-smaller");
+      rightCol.classList.add("right-col-smaller");
+    } else {
+      rightCol.classList.remove("right-col-smaller");
+      leftCol.classList.remove("left-col-smaller");
+    }
+  }
+}
+
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      positionColumns(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
